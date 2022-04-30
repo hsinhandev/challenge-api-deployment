de92ea76b11d00dc5d203e3f9cc254450a52c64e
Hsinhan Lin <13601628+hsinhandev@users.noreply.github.com>
2022-04-30 18:08:15 +0200

    Code refactoring

--- preprocessing/cleaning_data.py
+++ preprocessing/cleaning_data.py
@@ -17,17 +17,19 @@ def preprocess(payload: dict):
             message = err["msg"]
             response.append({"field": field, "message": message})
         return {"prediction": None, "error": response}
+    else:
+        df = pd.DataFrame(dict(_query), index=[0])
+        df.replace({False: 0, True: 1}, inplace=True)
+        df = df.replace(
+            {
+                "kitchen_type": kitchen_type_map,
+                "building_condition": building_condition_map,
+            }
+        )
 
-    df = pd.DataFrame(dict(_query), index=[0])
-    df.replace({False: 0, True: 1}, inplace=True)
-    df = df.replace(
-        {
-            "kitchen_type": kitchen_type_map,
-            "building_condition": building_condition_map,
-        }
-    )
-
-    df = pd.get_dummies(df, columns=df.columns.intersection(dummies_field).to_list())
-    df = df.reindex(columns=model_columns, fill_value=0)
+        df = pd.get_dummies(
+            df, columns=df.columns.intersection(dummies_field).to_list()
+        )
+        df = df.reindex(columns=model_columns, fill_value=0)
 
     return {"prediction": df, "error": None}
